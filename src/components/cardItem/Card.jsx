1a9271e1c323abea304a4fcc01e490e1e606141b
Herman <smurovnemez@mail.ru>
2022-11-03 12:20:31 -0500

    feat: add sass setup

---
+++ src/components/cardItem/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card() {
+  //script
+  function noob() {
+    console.log("dfhfdh");
+  }
+  //template
+  return (
+    <>
+      <h1 className="color">ghk</h1>
+      <button onClick={noob}>hgk</button>
+    </>
+  );
+}
+
+export default Card;
