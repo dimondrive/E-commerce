9b887a8c83b013e5e269aae2a66f034bd051910d
Herman <smurovnemez@mail.ru>
2022-11-03 15:34:25 -0500

    feat: add card template jsx

--- src/components/cardItem/Card.jsx
+++ src/components/cardItem/Card.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useRef, createRef } from "react";
 
-function Card() {
+import "@/components/cardItem/card.scss";
+import "@/components/cardItem/cardMedia.scss";
+
+//images
+import iphoneImg from "@/assets/images/iphone-product.svg";
+
+export default function Card() {
   //script
-  function noob() {
-    console.log("dfhfdh");
-  }
+  const [discount, setDiscount] = useState(20);
+  const images = {
+    iphoneImg,
+  };
+
   //template
   return (
     <>
-      <h1 className="color">Card</h1>
+      <ul className="products clearfix">
+        <li className="product-container">
+          <div className="discount-label">
+            <span>-{discount}%</span>
+          </div>
+          <div className="product">
+            <img className="product-image" src={images.iphoneImg} alt="" />
+            <p></p>
+            <button></button>
+          </div>
+        </li>
+      </ul>
     </>
   );
 }
-
-export default Card;
