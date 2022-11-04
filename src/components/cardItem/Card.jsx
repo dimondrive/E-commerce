dab89e603233c650e87648344ec3330e4d870609
Herman <smurovnemez@mail.ru>
2022-11-04 07:13:57 -0500

    feat: label list-rendering

--- src/components/cardItem/Card.jsx
+++ src/components/cardItem/Card.jsx
@@ -5,29 +5,42 @@ import "@/components/cardItem/cardMedia.scss";
 
 //images
 import iphoneImg from "@/assets/images/iphone-product.svg";
+//
+
+const props = {
+  //simulation props
+  discount: [20],
+  images: iphoneImg,
+};
 
 export default function Card() {
-  //script
-  const [discount, setDiscount] = useState(20);
-  const images = {
-    iphoneImg,
-  };
+  function DiscountLabel(props) {
+    return (
+      <div className="discount-label">
+        <span>-{props.value}%</span>
+      </div>
+    );
+  }
 
-  //template
+  const discount = props.discount;
+  const discountItem = discount.map((number) => (
+    <DiscountLabel key={number.toString()} value={number} />
+  ));
+
   return (
     <>
-      <ul className="products clearfix">
-        <li className="product-container">
-          <div className="discount-label">
-            <span>-{discount}%</span>
-          </div>
-          <div className="product">
-            <img className="product-image" src={images.iphoneImg} alt="" />
-            <p></p>
-            <button></button>
-          </div>
-        </li>
-      </ul>
+      <li className="product-container">{discountItem}</li>
     </>
   );
 }
+
+//   <li className="product-container">
+//   <div className="discount-label">
+//     <span>-{discount}%</span>
+//   </div>
+//   <div className="product">
+//     <img className="product-image" src={images.iphoneImg} alt="" />
+//     <p></p>
+//     <button></button>
+//   </div>
+// </li>
