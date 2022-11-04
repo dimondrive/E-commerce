e4bb90ffd504eec66de8bbda04fbf824b1e1f520
Herman <smurovnemez@mail.ru>
2022-11-04 10:34:16 -0500

    feat: RenderListCard

--- src/components/cardItem/CardTemplate.jsx
+++ src/components/cardItem/CardTemplate.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, createRef } from "react";
 
+import iphoneImg from "@/assets/images/iphone-product.svg";
 import "@/components/cardItem/card.scss";
 import "@/components/cardItem/cardMedia.scss";
 
@@ -7,13 +8,36 @@ import "@/components/cardItem/cardMedia.scss";
 import RenderCard from "@/components/cardItem/RenderListCard";
 //
 
-export default function Card({ discount }) {
+export default function Card({
+  discount,
+  index,
+  images,
+  icon,
+  price,
+  oldPrice,
+}) {
   return (
     <>
       <ul>
         <li className="product-container">
           <div className="discount-label">
             <span>-{discount}%</span>
+          </div>
+          <div className="product">
+            <img className="product-image" src={images} alt="" />
+            <p className="cart-descr"></p>
+
+            <div className="product-price">
+              <p className="price">{price}</p>
+              <p className="price-old">{oldPrice}</p>
+            </div>
+          </div>
+
+          <div className="cart">
+            <button className="cart-btn">
+              <p className="cart-descr">В корзину</p>
+            </button>
+            <img className="cart-img" src={icon} alt="" />
           </div>
         </li>
       </ul>
--- src/components/cardItem/RenderListCard.jsx
+++ src/components/cardItem/RenderListCard.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useRef, createRef } from "react";
 
-import Card from "@/components/cardItem/Card";
+//template
+import Card from "@/components/cardItem/CardTemplate";
+
+//images
+import iphoneImg from "@/assets/images/iphone-product.svg";
+
+// icons
+import basketIcon from "@/assets/icons/fa_shopping-basket.svg";
 
 export default function RenderCard() {
   const data = [
     {
       discount: "20",
       description: "Смартфон Samsung Galaxy S21 5G 8GB/128GB",
+      images: iphoneImg,
+      icon: basketIcon,
+      price: "2099",
+      priceOld: "2499",
+    },
+    {
+      discount: "30",
+      description: "Смартфон Samsung Galaxy S21 5G 8GB/128GB",
+      images: iphoneImg,
+      icon: basketIcon,
+      price: "2099",
+      priceOld: "2499",
     },
   ];
   return (
@@ -14,8 +33,12 @@ export default function RenderCard() {
       {data.map((item, index) => (
         <Card
           discount={item.discount}
+          price={item.price}
           description={item.description}
           key={index}
+          images={item.images}
+          icon={item.icon}
+          oldPrice={item.priceOld}
         />
       ))}
     </>
