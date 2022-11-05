72bef3b7a1ee3b24e3ef66a4e1a653f6de8dd70f
Herman <smurovnemez@mail.ru>
2022-11-05 17:51:12 -0500

    feat: add cors at express server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import data from './data.js';
-
+import cors  from 'cors';
 const app = express()
+
+
+
+app.use(cors({
+    origin: '*'
+}))
 
 app.get('/api/products', (req, res) => {
     res.send(data.products)
-}) 
+});
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
